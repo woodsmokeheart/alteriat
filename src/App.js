140081ca7c7d8cd11445f3863dd4166721d1db9f
Denis Kukobin <denis.kukobin@hipasus.com>
2024-10-01 14:12:23 +0300

    two

--- src/App.js
+++ src/App.js
@@ -1,36 +1,103 @@
 import React, { useEffect, useState } from "react";
-import "./App.css";
 
-function App() {
-  const [user, setUser] = useState(null);
+const App = () => {
+  const [isVisible, setIsVisible] = useState(true);
+  const [isActive, setIsActive] = useState(true);
+  const [user, setUser] = useState({});
+
   useEffect(() => {
-    const initTelegram = () => {
-      if (window.Telegram && window.Telegram.WebApp) {
-        const userData = window.Telegram.WebApp.initDataUnsafe.user;
-        if (userData) {
-          setUser(userData);
-        }
-      }
-    };
+    const tg = window.Telegram.WebApp; // Получаем объект webapp телеграма
 
-    initTelegram();
+    tg.expand(); // Расширяем на все окно
+
+    tg.MainButton.setText("Changed Text1");
+    tg.MainButton.textColor = "#F55353"; // Меняем цвет текста кнопки
+    tg.MainButton.color = "#143F6B"; // Меняем цвет фона кнопки
+    tg.MainButton.setParams({ color: "#143F6B" });
+
+    setUser(tg.initDataUnsafe.user); // Сохраняем данные пользователя
+
+    tg.onEvent("mainButtonClicked", () => {
+      tg.sendData("some string that we need to send");
+    });
   }, []);
+
+  const toggleMainButton = () => {
+    const tg = window.Telegram.WebApp;
+    if (isVisible) {
+      tg.MainButton.hide();
+    } else {
+      tg.MainButton.show();
+    }
+    setIsVisible(!isVisible);
+  };
+
+  const toggleEnableMainButton = () => {
+    const tg = window.Telegram.WebApp;
+    if (isActive) {
+      tg.MainButton.setParams({ color: "#E0FFFF" });
+      tg.MainButton.disable();
+    } else {
+      tg.MainButton.setParams({ color: "#143F6B" });
+      tg.MainButton.enable();
+    }
+    setIsActive(!isActive);
+  };
+
   return (
-    <div>
-      {user ? (
-        <div>
-          <h1>
-            Привет, {user.first_name} {user.last_name}!
-          </h1>
-          {user.photo_url && (
-            <img src={user.photo_url} alt="Аватар пользователя" />
-          )}
-        </div>
-      ) : (
-        <h1>Загрузка...</h1>
-      )}
+    <div
+      style={{
+        color: "var(--tg-theme-text-color)",
+        backgroundColor: "var(--tg-theme-bg-color)",
+        textAlign: "center",
+        fontSize: "18px",
+      }}
+    >
+      <div id="usercard">
+        <p>
+          {user.first_name} {user.last_name} {user.username} (
+          {user.language_code})
+        </p>
+        <p>{user.id}</p>
+      </div>
+      <p>Just text</p>
+      <a
+        className="link"
+        href="https://mihailgok.ru"
+        style={{ color: "var(--tg-theme-link-color)" }}
+      >
+        Link
+      </a>
+      <p className="hint" style={{ color: "var(--tg-theme-hint-color)" }}>
+        Some little hint
+      </p>
+      <button
+        className="button"
+        onClick={toggleMainButton}
+        style={{
+          background: "var(--tg-theme-button-color)",
+          color: "var(--tg-theme-button-text-color)",
+          border: "none",
+          fontSize: "18px",
+          marginBottom: "20px",
+        }}
+      >
+        Show/Hide Main Button
+      </button>
+      <button
+        className="button"
+        onClick={toggleEnableMainButton}
+        style={{
+          background: "var(--tg-theme-button-color)",
+          color: "var(--tg-theme-button-text-color)",
+          border: "none",
+          fontSize: "18px",
+        }}
+      >
+        Enable/Disable Main Button
+      </button>
     </div>
   );
-}
+};
 
 export default App;
