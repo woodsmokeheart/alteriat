588cd1af8d8932be74c31736b408e82da18b0939
Denis Kukobin <denis.kukobin@hipasus.com>
2024-10-02 14:38:31 +0300

    added new styles from bottom bar

--- src/components/BottomBar/BottomBar.jsx
+++ src/components/BottomBar/BottomBar.jsx
@@ -1,14 +1,54 @@
 import React from "react";
-import "./BottomBar.css"; // Подключим стили для кнопок
+import css from "./BottomBar.module.css"; // Подключим стили для кнопок
 
 export const BottomBar = () => {
   return (
-    <div className="bottom-bar">
-      <button className="bottom-button">Profile</button>
-      <button className="bottom-button">Boosters</button>
-      <button className="bottom-button">Earn</button>
-      <button className="bottom-button">Games</button>
+    <div className={css.bottom_bar}>
+      <button className={css.bottom_button}>
+        <img
+          width="34"
+          height="34"
+          src="https://img.icons8.com/arcade/64/armored-helmet.png"
+          alt="armored-helmet"
+        />
+        Profile
+      </button>
+      <button className={css.bottom_button}>
+        <img
+          width="34"
+          height="34"
+          src="https://img.icons8.com/arcade/64/mana.png"
+          alt="mana"
+        />
+        Boosters
+      </button>
+      <button className={css.bottom_button}>
+        <img
+          width="34"
+          height="34"
+          src="https://img.icons8.com/arcade/64/medieval-barracks.png"
+          alt="medieval-barracks"
+        />
+        English
+      </button>
+      <button className={css.bottom_button}>
+        <img
+          width="34"
+          height="34"
+          src="https://img.icons8.com/arcade/64/treasure-chest.png"
+          alt="treasure-chest"
+        />
+        Earn
+      </button>
+      <button className={css.bottom_button}>
+        <img
+          width="34"
+          height="34"
+          src="https://img.icons8.com/arcade/64/joystick.png"
+          alt="joystick"
+        />
+        Games
+      </button>
     </div>
   );
 };
-
