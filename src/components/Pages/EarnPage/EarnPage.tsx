be2c4079b89b6c4c1aa018b8dba19700a0be749d
Denis Kukobin <denis.kukobin@hipasus.com>
2024-10-06 13:29:29 +0300

    added parse channel

--- src/components/Pages/EarnPage/EarnPage.tsx
+++ src/components/Pages/EarnPage/EarnPage.tsx
@@ -9,41 +9,16 @@ interface Channel {
   isSubscribed: boolean; // состояние подписки
 }
 
-const BOT_TOKEN = "YOUR_BOT_TOKEN"; // Замените на ваш действующий токен
-const CHANNEL_ID = "-1001688696412"; // ID вашего канала
+const BOT_TOKEN = "7891645977:AAHxkkMue5K7IL-5qog57MSEWRmmlm4ET8w";
+const CHANNEL_ID = "-1001688696412";
 
 const CHANNEL_API_URL = `https://api.telegram.org/bot${BOT_TOKEN}/getChat?chat_id=${CHANNEL_ID}`;
-
-// Асинхронная функция для проверки подписки
-const checkSubscription = async (userId: number) => {
-  const response = await fetch(
-    `https://api.telegram.org/bot${BOT_TOKEN}/getChatMember?chat_id=${CHANNEL_ID}&user_id=${userId}`
-  );
-  const data = await response.json();
-  if (
-    data.ok &&
-    (data.result.status === "member" ||
-      data.result.status === "administrator" ||
-      data.result.status === "creator")
-  ) {
-    return true;
-  }
-  return false;
-};
 
 export const EarnPage = () => {
   const [channel, setChannel] = useState<Channel | null>(null);
   const [isSubscribed, setIsSubscribed] = useState(false);
-  const [userId, setUserId] = useState<number | null>(null); // ID пользователя
 
   useEffect(() => {
-    // Имитируем получение userId через Telegram WebApp API
-    const initDataUnsafe = (window as any).Telegram.WebApp.initDataUnsafe;
-    const userId = initDataUnsafe?.user?.id;
-    if (userId) {
-      setUserId(userId); // Сохраняем userId в состоянии
-    }
-
     // Получаем информацию о канале
     fetch(CHANNEL_API_URL)
       .then((response) => response.json())
@@ -62,22 +37,10 @@ export const EarnPage = () => {
       .catch((error) => console.error("Error fetching channel data:", error));
   }, []);
 
-  // Проверка подписки пользователя
-  const checkUserSubscription = async () => {
-    if (userId) {
-      const subscribed = await checkSubscription(userId);
-      setIsSubscribed(subscribed); // Обновляем состояние подписки
-    } else {
-      console.error("User ID не найден");
-    }
-  };
-
   const handleSubscribe = () => {
     // Логика подписки (например, редирект на Telegram)
     window.open("https://t.me/alteriat", "_blank");
-    setTimeout(() => {
-      checkUserSubscription(); // Проверяем подписку после некоторого времени
-    }, 5000); // Даем 5 секунд на подписку
+    setIsSubscribed(true); // Предполагаем, что подписка произошла успешно
   };
 
   return (
