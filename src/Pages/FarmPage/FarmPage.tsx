11767488e94227a7e682e89d70e460e6872d42aa
Denis Kukobin <denis.kukobin@hipasus.com>
2024-12-21 22:26:11 +0300

    rework amination

--- src/Pages/FarmPage/FarmPage.tsx
+++ src/Pages/FarmPage/FarmPage.tsx
@@ -1,14 +1,22 @@
+import { useState } from "react";
 import { Footer } from "components/Footer/Footer";
-
-import css from "./FarmPage.module.css";
 import { IconFingerprint } from "assets/svg/IconFingerprint";
 import { HeaderFarm } from "components/HeaderFarm/HeaderFarm";
+import css from "./FarmPage.module.css";
 
 export const FarmPage = () => {
+  const [isPressed, setIsPressed] = useState(false);
   return (
     <div className={css.wrapper}>
       <HeaderFarm />
-      <button className={css.fingerprint}>
+      <button
+        className={`${css.fingerprint} ${isPressed ? css.active : ""}`}
+        onMouseDown={() => setIsPressed(true)}
+        onMouseUp={() => setIsPressed(false)}
+        onMouseLeave={() => setIsPressed(false)}
+        onTouchStart={() => setIsPressed(true)}
+        onTouchEnd={() => setIsPressed(false)}
+      >
         <IconFingerprint width={100} height={100} />
       </button>
       <Footer />
