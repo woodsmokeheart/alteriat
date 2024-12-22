d4c76df7f4d6a8e4c6336ebd9b2d4f672a2c3ca5
Denis Kukobin <denis.kukobin@hipasus.com>
2024-12-22 15:38:15 +0300

    added facts

--- src/Pages/FarmPage/FarmPage.tsx
+++ src/Pages/FarmPage/FarmPage.tsx
@@ -6,10 +6,36 @@ import css from "./FarmPage.module.css";
 import { TriangleSpinner } from "components/Loaders/Triangle/TriangleSpinner";
 import React from "react";
 
+const facts = [
+  "Черный квадрат нарисовал Малевич",
+  "2 x 2 = 4",
+  "Скорость света 299 792 км/с",
+  "Площадь круга равна πr²",
+  "Солнце — это звезда",
+];
+
 export const FarmPage = () => {
   const [isLoader, setIsLoader] = useState(false);
   const [isPressed, setIsPressed] = useState(false);
 
+  const [currentFact, setCurrentFact] = useState(0);
+  const [factInterval, setFactInterval] = useState<number | null>(null);
+
+  const handlePressStart = () => {
+    setIsPressed(true);
+    const interval = window.setInterval(() => {
+      setCurrentFact((prevFact) => (prevFact + 1) % facts.length);
+    }, 1000);
+    setFactInterval(interval);
+  };
+
+  const handlePressEnd = () => {
+    setIsPressed(false);
+    if (factInterval !== null) {
+      clearInterval(factInterval);
+      setFactInterval(null);
+    }
+  };
   useEffect(() => {
     setIsLoader(true);
 
@@ -25,17 +51,22 @@ export const FarmPage = () => {
       ) : (
         <React.Fragment>
           <HeaderFarm />
-          <div className={css.fingerprint_container}>
-            <button
-              className={`${css.fingerprint} ${isPressed ? css.active : ""}`}
-              onMouseDown={() => setIsPressed(true)}
-              onMouseUp={() => setIsPressed(false)}
-              onMouseLeave={() => setIsPressed(false)}
-              onTouchStart={() => setIsPressed(true)}
-              onTouchEnd={() => setIsPressed(false)}
-            >
-              <IconFingerprint width={100} height={100} />
-            </button>
+          <div className={css.content}>
+            <div className={css.carousel_facts}>
+              <p>{facts[currentFact]}</p>
+            </div>
+            <div className={css.fingerprint_container}>
+              <button
+                className={`${css.fingerprint} ${isPressed ? css.active : ""}`}
+                onMouseDown={handlePressStart}
+                onMouseUp={handlePressEnd}
+                onMouseLeave={handlePressEnd}
+                onTouchStart={handlePressStart}
+                onTouchEnd={handlePressEnd}
+              >
+                <IconFingerprint width={100} height={100} />
+              </button>
+            </div>
           </div>
           <Footer />
         </React.Fragment>
