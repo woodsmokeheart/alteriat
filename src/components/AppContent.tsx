4eacb5c9195438391f025208318a64e44fac3343
Denis Kukobin <denis.kukobin@hipasus.com>
2025-05-07 15:34:25 +0300

    imports

--- src/components/AppContent.tsx
+++ src/components/AppContent.tsx
@@ -1,4 +1,5 @@
-import React, { memo } from 'react';
+import * as React from 'react';
+import { memo } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { useProfileContext } from '../context/ProfileContext';
 import Login from '../pages/Login';
